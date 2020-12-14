de27c64a84aa4f602645701f62f2e25745e17a1d
joseph danna <jodyanna14.15@gmail.com>
2020-12-14 14:48:41 -0600

    function to convert chars to hex

--- src/components/TextareaForm/util.js
+++ src/components/TextareaForm/util.js
@@ -1,7 +1,6 @@
 
-
 /**
- * Converts hex to integer
+ * Converts hexes to integers
  * @param {Array<string>} hexes - Array of hex values
  * @returns {Array<number>}
  */
@@ -14,14 +13,20 @@ const convertHexesToNums = hexes => {
 }
 
 /**
- *
- * @param {Array<string>} kanji
+ * Converts single character strings to hexes
+ * @param {Array<string>} chars - Array of single character strings
  * @returns {Array<string>}
  */
-const convertKanjiToHexes = kanji => {
-
+const convertCharsToHexes = chars => {
+  const hexes = [];
+  for (let i = 0; i < chars.length; i++) {
+    hexes[i] = chars[i].charCodeAt(0).toString(16)
+  }
+  return hexes
 }
 
+// export functions as object
 module.exports = {
   convertHexesToNums: convertHexesToNums,
+  convertCharsToHexes: convertCharsToHexes,
 }
--- src/components/TextareaForm/util.test.js
+++ src/components/TextareaForm/util.test.js
@@ -5,3 +5,9 @@ test("convertHexesToNums", () => {
   const numArray = [14, 254, 2748, 19968, 638720];
   expect(util.convertHexesToNums(hexArray)).toStrictEqual(numArray);
 })
+
+test("convertCharsToHexes", () => {
+  const charArray = ['a', 'æ', '心'];
+  const hexArray = ['61', 'e6', '5fc3'];
+  expect(util.convertCharsToHexes(charArray)).toStrictEqual(hexArray);
+})
