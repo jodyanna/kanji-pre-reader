ea7db583fa7c6fcfdda9828eead01af0d043bde3
joseph danna <jodyanna14.15@gmail.com>
2020-12-15 20:41:28 -0600

    way easier with regex

--- src/components/TextareaForm/index.js
+++ src/components/TextareaForm/index.js
@@ -3,20 +3,15 @@ import { Form, Textarea } from "./style";
 
 export default function TextareaForm() {
   const [ text, setText ] = useState("Paste text here.");
+  const [ kanji, setKanji ] = useState([]);
 
   const handleChange = event => setText(event.target.value);
 
   const handleSubmit = event => {
     event.preventDefault();
+    setKanji(parseKanjiFromText(text));
 
-    // convert text to unicode hex
-    console.log(text.charCodeAt(0).toString(16));
-
-    // convert hex to int
-    console.log("hex to num: " + parseInt("4dbf", 16));
-
-    // all common kanji hex values 4e00 - 9faf // int num (19968 - 40879)
-    // all rare kanji hex values 3400 - 4dbf // int num (13312 - 19903)
+    console.log(kanji)
   }
 
   return (
@@ -30,4 +25,10 @@ export default function TextareaForm() {
       <input type="submit" value="Create study sheet" />
     </Form>
   )
+}
+
+const parseKanjiFromText = text => {
+  const kanjiRegX = /[\u4e00-\u9faf\u3400-\u4dbf]/g
+
+  return [...new Set(text.match(kanjiRegX))]
 }
--- src/components/TextareaForm/util.js
+++ src/components/TextareaForm/util.js
@@ -1,10 +1,11 @@
 
 /**
- * Converts hexes to integers
- * @param {Array<string>} hexes - Array of hex values
+ * Converts hexes to integers.
+ * @param {Array<string>} hexes - Array of hex values.
  * @returns {Array<number>}
  */
 const convertHexesToNums = hexes => {
+  if (!Array.isArray(hexes)) throw new Error("Param is not an Array");
   const nums = [];
   for (let i = 0; i < hexes.length; i++) {
     if (typeof hexes[i] === "string") nums[i] = parseInt(hexes[i], 16)
@@ -13,20 +14,33 @@ const convertHexesToNums = hexes => {
 }
 
 /**
- * Converts single character strings to hexes
- * @param {Array<string>} chars - Array of single character strings
+ * Converts single character strings to unicode hexes.
+ * @param {Array<string>} chars - Array of single character strings.
  * @returns {Array<string>}
  */
 const convertCharsToHexes = chars => {
+  if (!Array.isArray(chars)) throw new Error("Param is not an Array");
   const hexes = [];
   for (let i = 0; i < chars.length; i++) {
-    hexes[i] = chars[i].charCodeAt(0).toString(16)
+    if (typeof chars[i] === "string") hexes[i] = chars[i].charCodeAt(0).toString(16)
   }
   return hexes
+}
+
+/**
+ * Parse kanji characters from string into array.
+ * @param {string} text -
+ * @returns {Array<string>} array of single non-repeating kanji characters.
+ */
+const parseKanjiFromText = text => {
+  const kanjiRegX = /[\u4e00-\u9faf\u3400-\u4dbf]/g
+
+  return [...new Set(text.match(kanjiRegX))]
 }
 
 // export functions as object
 module.exports = {
   convertHexesToNums: convertHexesToNums,
   convertCharsToHexes: convertCharsToHexes,
+  parseKanjiFromText: parseKanjiFromText,
 }
