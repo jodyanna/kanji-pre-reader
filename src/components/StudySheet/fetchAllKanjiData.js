b73b133ad4e1cd4fb64368daa6a7bfb0f85a824c
joseph danna <jodyanna14.15@gmail.com>
2021-01-02 15:26:03 -0600

    comments and moving func

--- src/components/StudySheet/fetchAllKanjiData.js
+++ src/components/StudySheet/fetchAllKanjiData.js
@@ -13,3 +13,32 @@
  * @property {string} unicode - Hex value for unicode.
  * @property {string} heisig_en - Different English translation?
  */
+
+/**
+ * Make a fetch request to kanjiapi.dev for each Kanji char parsed from TextareaForm component.
+ * @async
+ * @function fetchAllKanjiData
+ * @param {Array<string>} kanji - An Array of valid single char strings of Kanji used for API call.
+ * @returns {Array<Promise<KanjiData>>} allKanjiData - An Array holding API response JSON objects.
+ */
+export const fetchAllKanjiData = async kanji => {
+  /** @type {Array<Promise<KanjiData>>} */
+  const allKanjiData = [];
+
+  for (let i = 0; i < kanji.length; i++) {
+    /** @type {Response} */
+    const response = await fetch("https://kanjiapi.dev/v1/kanji/" + kanji[i])
+      .catch((error) => console.error('Error:', error));
+
+    if (response.ok) {
+      /** @type {Promise<KanjiData>} */
+      const data = await response.json();
+      allKanjiData.push(data)
+    }
+    else {
+      return Promise.reject("Could not connect to API.")
+    }
+  }
+
+  return allKanjiData
+}
--- src/components/StudySheet/index.js
+++ src/components/StudySheet/index.js
@@ -4,7 +4,7 @@ import { Page } from "./style";
 import StudySheetEntry from "../StudySheetEntry";
 import html2canvas from "html2canvas";
 import { jsPDF } from "jspdf";
-
+import { fetchAllKanjiData } from "./fetchAllKanjiData";
 
 
 export default function StudySheet(props) {
@@ -80,24 +80,7 @@ export default function StudySheet(props) {
   )
 }
 
-const fetchAllKanjiData = async kanji => {
-  const allKanjiData = [];
 
-  for (let i = 0; i < kanji.length; i++) {
-    const response = await fetch("https://kanjiapi.dev/v1/kanji/" + kanji[i])
-      .catch((error) => console.error('Error:', error));
-
-    if (response.ok) {
-      const data = await response.json();
-      allKanjiData.push(data)
-    }
-    else {
-      return Promise.reject("Could not connect to API.")
-    }
-  }
-
-  return allKanjiData
-}
 
 const printDoc = () => {
   html2canvas(document.querySelector("#pdf-0"))
