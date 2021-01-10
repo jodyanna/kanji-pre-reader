19b9244bac5ba53a2d6d83da68cd90bea303182d
joseph danna <jodyanna14.15@gmail.com>
2021-01-10 16:47:15 -0600

    comments

--- src/components/TextareaForm/parseKanjiFromText.js
+++ src/components/TextareaForm/parseKanjiFromText.js
@@ -1,5 +1,10 @@
-
+/**
+ * Parse Kanji characters from user text input (from TextareaForm). Remove duplicates.
+ * @param {string} text
+ * @returns {Array<string>}
+ */
 export const parseKanjiFromText = text => {
+  /** @type {RegExp} */
   const kanjiRegX = /[\u4e00-\u9faf\u3400-\u4dbf]/g
 
   // use set to remove duplicates
