767578ade1a259e5bffb2bfad85747af670ae42b
joseph danna <jodyanna14.15@gmail.com>
2021-01-12 22:49:41 -0600

    shared object for coloring

---
+++ src/components/Shared/colorScheme.js
@@ -0,0 +1,12 @@
+/**
+ * Object for coloring styled-components.
+ * @type {{red: string, green: string, blue: string, white: string, black: string, purple: string}}
+ */
+export const colors = {
+  purple: "#841983",
+  red: "#ec1f4d",
+  green: "#a4d555",
+  blue: "#4cdef5",
+  white: "#ffffff",
+  black: "#000000",
+}
